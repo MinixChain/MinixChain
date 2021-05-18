d8b8fc3f79c1fdeee39ef0af9b3a533d63368468
郭光华 <guanghuaguo.oss@gmail.com>
2021-05-18 15:40:51 +0800

    Develop (#14)
    * Fix/coming (#10)
    
    * Bug: Fix build error
    
    * Refactor NotFoundBondType
    
    * Add Properties in genesis config
    
    Co-authored-by: 郭光华 <guanghuaguo.oss@gmail.com>
    Co-authored-by: gguoss <1536310027@qq.com>
    Co-authored-by: icodezjb <icodezjb@users.noreply.github.com>
    
    * Fix/coming (#10)
    
    * Bug: Fix build error
    
    * Refactor NotFoundBondType
    
    * Add Properties in genesis config
    
    Co-authored-by: 郭光华 <guanghuaguo.oss@gmail.com>
    Co-authored-by: gguoss <1536310027@qq.com>
    Co-authored-by: icodezjb <icodezjb@users.noreply.github.com>
    
    * feature: Add admin key level (#11)
    
    * feature: Add admin key level
    
    * bug: Fix test case build error
    
    * Bug/genesis (#13)
    
    * feature: Add admin key level
    
    * bug: Fix test case build error
    
    * Error: fix genesis build error.
    
    Co-authored-by: icodezjb <8869892+icodezjb@users.noreply.github.com>
    Co-authored-by: icodezjb <icodezjb@users.noreply.github.com>

--- bin/minix/node/src/chain_spec.rs
+++ bin/minix/node/src/chain_spec.rs
@@ -1,12 +1,12 @@
-use sp_core::{Pair, Public, sr25519};
 pub use minix_runtime::{
-	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
-	SudoConfig, SystemConfig, /*NFTConfig,*/ ComingIdConfig, WASM_BINARY, Signature
+    AccountId, AuraConfig, BalancesConfig, /*NFTConfig,*/ ComingIdConfig, GenesisConfig,
+    GrandpaConfig, Signature, SudoConfig, SystemConfig, WASM_BINARY,
 };
+use sc_service::{ChainType, Properties};
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+use sp_core::{sr25519, Pair, Public};
 use sp_finality_grandpa::AuthorityId as GrandpaId;
-use sp_runtime::traits::{Verify, IdentifyAccount};
-use sc_service::{ChainType,Properties};
+use sp_runtime::traits::{IdentifyAccount, Verify};
 
 // The URL for the telemetry server.
 // const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
@@ -16,62 +16,60 @@ pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
 
 /// Generate a crypto pair from seed.
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
-	TPublic::Pair::from_string(&format!("//{}", seed), None)
-		.expect("static values are valid; qed")
-		.public()
+    TPublic::Pair::from_string(&format!("//{}", seed), None)
+        .expect("static values are valid; qed")
+        .public()
 }
 
 type AccountPublic = <Signature as Verify>::Signer;
 
 /// Generate an account ID from seed.
-pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
-	AccountPublic: From<<TPublic::Pair as Pair>::Public>
+pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId
+where
+    AccountPublic: From<<TPublic::Pair as Pair>::Public>,
 {
-	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
+    AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
 
 /// Generate an Aura authority key.
 pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
-	(
-		get_from_seed::<AuraId>(s),
-		get_from_seed::<GrandpaId>(s),
-	)
+    (get_from_seed::<AuraId>(s), get_from_seed::<GrandpaId>(s))
 }
 
 pub fn development_config() -> Result<ChainSpec, String> {
-	Ok(ChainSpec::from_genesis(
-		// Name
-		"Development",
-		// ID
-		"dev",
-		ChainType::Development,
-		move || minix_genesis(
-			// Initial PoA authorities
-			vec![
-				authority_keys_from_seed("Alice"),
-			],
-			// Sudo account
-			get_account_id_from_seed::<sr25519::Public>("Alice"),
-			// Pre-funded accounts
-			vec![
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				get_account_id_from_seed::<sr25519::Public>("Bob"),
-				get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-			],
-			true,
-		),
-		// Bootnodes
-		vec![],
-		// Telemetry
-		None,
-		// Protocol ID
-		None,
-		// Properties
-		None,
-		// Extensions
-		None,
-	))
+    Ok(ChainSpec::from_genesis(
+        // Name
+        "Development",
+        // ID
+        "dev",
+        ChainType::Development,
+        move || {
+            minix_genesis(
+                // Initial PoA authorities
+                vec![authority_keys_from_seed("Alice")],
+                // Sudo account
+                get_account_id_from_seed::<sr25519::Public>("Alice"),
+                // Pre-funded accounts
+                vec![
+                    get_account_id_from_seed::<sr25519::Public>("Alice"),
+                    get_account_id_from_seed::<sr25519::Public>("Bob"),
+                    get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+                ],
+                true,
+            )
+        },
+        // Bootnodes
+        vec![],
+        // Telemetry
+        None,
+        // Protocol ID
+        None,
+        // Properties
+        None,
+        // Extensions
+        None,
+    ))
 }
 
 pub fn local_testnet_config() -> Result<ChainSpec, String> {
@@ -125,39 +123,47 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 
 /// Configure initial storage state for FRAME modules.
 pub fn minix_genesis(
-	initial_authorities: Vec<(AuraId, GrandpaId)>,
-	root_key: AccountId,
-	endowed_accounts: Vec<AccountId>,
-	_enable_println: bool,
+    initial_authorities: Vec<(AuraId, GrandpaId)>,
+    root_key: AccountId,
+    endowed_accounts: Vec<AccountId>,
+    _enable_println: bool,
 ) -> GenesisConfig {
-	let wasm_binary = WASM_BINARY.unwrap();
-	GenesisConfig {
-		frame_system: SystemConfig {
-			// Add Wasm runtime to storage.
-			code: wasm_binary.to_vec(),
-			changes_trie_config: Default::default(),
-		},
-		pallet_balances: BalancesConfig {
-			// Configure endowed accounts with initial balance of 1 << 60.
-			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
-		},
-		pallet_aura: AuraConfig {
-			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
-		},
-		pallet_grandpa: GrandpaConfig {
-			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
-		},
-		pallet_sudo: SudoConfig {
-			// Assign network admin rights.
-			key: root_key.clone(),
-		},
-		pallet_coming_id: ComingIdConfig {
-			// Assign network admin rights.
-			admin_key: root_key,
-		},
-
+    let wasm_binary = WASM_BINARY.unwrap();
+    GenesisConfig {
+        frame_system: SystemConfig {
+            // Add Wasm runtime to storage.
+            code: wasm_binary.to_vec(),
+            changes_trie_config: Default::default(),
+        },
+        pallet_balances: BalancesConfig {
+            // Configure endowed accounts with initial balance of 1 << 60.
+            balances: endowed_accounts
+                .iter()
+                .cloned()
+                .map(|k| (k, 1 << 60))
+                .collect(),
+        },
+        pallet_aura: AuraConfig {
+            authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
+        },
+        pallet_grandpa: GrandpaConfig {
+            authorities: initial_authorities
+                .iter()
+                .map(|x| (x.1.clone(), 1))
+                .collect(),
+        },
+        pallet_sudo: SudoConfig {
+            // Assign network admin rights.
+            key: root_key.clone(),
+        },
+        pallet_coming_id: ComingIdConfig {
+            // Assign network admin rights.
+            high_admin_key: root_key.clone(),
+            medium_admin_key: root_key.clone(),
+            low_admin_key: root_key,
+        },
         /*pallet_commodities: NFTConfig {
             balances: Vec::new(),
         },*/
-	}
+    }
 }

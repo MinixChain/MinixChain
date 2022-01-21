06d26641c8a62bb60c753690b2a49a9faeef3fd3
icodezjb <icodezjb@users.noreply.github.com>
2022-01-21 09:57:48 +0800

    Add srtool support

--- pallets/threshold-signature/src/lib.rs
+++ pallets/threshold-signature/src/lib.rs
@@ -57,7 +57,7 @@ pub mod pallet {
     /// Configure the pallet by specifying the parameters and types on which it depends.
     #[pallet::config]
     pub trait Config: frame_system::Config + pallet_balances::Config {
-        /// Because this pallet emits events, it depends on the runtime's definition of an event.
+        /// Because this pallet emits events, it depends on the minix's definition of an event.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
         /// A dispatchable call.
         type Call: Parameter
